626391440b081ba24fcfb4298d6a6dadc7c0fdc5
Thai Nguyen <thai.nguyen@aegona.com>
2023-07-12 13:21:30 +0700

    #

--- wp-content/themes/flatsome-child/components/home_projects.php
+++ wp-content/themes/flatsome-child/components/home_projects.php
@@ -7,18 +7,19 @@
             'orderby' => 'id'
         ]);
         ?>
-        <?php foreach ($terms as $value) : ?>
-            <div class="category">
+        <?php foreach ($terms as $key => $value) : ?>
+            <div class="category <?= $key == 0 ? "active" : "" ?>" data-categoryid="<?= @$value->term_id ?>">
                 <img src="<?= THEME_URL . '/assets/images/but.png' ?>" alt="but">
                 <span><?= @$value->name ?></span>
             </div>
         <?php endforeach ?>
     </div>
-    <?php foreach ($terms as $term) : ?>
+    <?php foreach ($terms as $key => $term) : ?>
         <?php
         $projects = get_posts(array(
             'post_type' => 'project',
-            'numberposts' => -1,
+            'numberposts' => 32,
+            'orderby' => '-id',
             'tax_query' => array(
                 array(
                     'taxonomy' => 'project_category',
@@ -29,10 +30,10 @@
             )
         ));
         ?>
-        <div class="projects">
+        <div class="projects <?= $key == 0 ? "active" : "" ?>" data-categorycontentid="<?= @$term->term_id ?>">
             <?php foreach ($projects as $project) : ?>
                 <?php $data = get_post_meta($project->ID); ?>
-                <a href="" class="project">
+                <a href="<?= get_permalink($project) ?>" class="project">
                     <div class="image">
                         <img src="<?= wp_get_attachment_image_url(@$data['images'][0], 'full') ?>" alt="project">
                     </div>
@@ -46,5 +47,23 @@
             <?php endforeach ?>
         </div>
     <?php endforeach ?>
-
 </div>
+<script>
+    let categories = document.querySelectorAll("#home_projects .category")
+    categories?.forEach(e => {
+        e?.addEventListener("click", function(x) {
+            categories?.forEach(e => {
+                e?.classList?.remove("active")
+            })
+            e?.classList?.add("active")
+            let id = e?.getAttribute("data-categoryid")
+            let projects = document.querySelectorAll("#home_projects .projects")
+            let categoryContent = document.querySelector(`.projects[data-categorycontentid="${id}"]`)
+            console.log(id, projects, categoryContent)
+            projects?.forEach(e => {
+                e?.classList?.remove("active")
+            })
+            categoryContent?.classList?.add("active")
+        })
+    })
+</script>
