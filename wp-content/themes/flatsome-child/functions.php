82ca180eb8b404de5a91df5164d86df4b31f2a82
Thai Nguyen <thai.nguyen@aegona.com>
2023-08-14 10:31:30 +0700

    up

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -16,7 +16,7 @@ function is_bot()
 
 function add_favicon()
 {
-    ?>
+?>
     <link rel="apple-touch-icon" sizes="57x57" href="<?= THEME_URL . "/assets" ?>/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?= THEME_URL . "/assets" ?>/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="<?= THEME_URL . "/assets" ?>/apple-icon-72x72.png">
@@ -26,7 +26,7 @@ function add_favicon()
     <link rel="apple-touch-icon" sizes="144x144" href="<?= THEME_URL . "/assets" ?>/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="<?= THEME_URL . "/assets" ?>/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="<?= THEME_URL . "/assets" ?>/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= THEME_URL . "/assets" ?>/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= THEME_URL . "/assets" ?>/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?= THEME_URL . "/assets" ?>/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="<?= THEME_URL . "/assets" ?>/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?= THEME_URL . "/assets" ?>/favicon-16x16.png">
@@ -35,39 +35,32 @@ function add_favicon()
     <meta name="msapplication-TileImage" content="<?= THEME_URL . "/assets" ?>/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <meta name="author" content="author" />
-    <?php
+<?php
 }
 add_action('wp_head', 'add_favicon', 0);
 
 function add_styles()
 {
-
-    ?>
+?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-    <?php
+<?php
 
 }
-if (!is_bot()) {
-    add_action('wp_head', 'add_styles', 999999999);
-}
+add_action('wp_head', 'add_styles', 999999999);
 
 function add_js()
 {
-    ?>
+?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
     <script src="<?= THEME_URL . '/assets/js/main.js' ?>"></script>
-    <?php
+<?php
 }
-if (!is_bot()) {
-    add_action('wp_footer', 'add_js', 999999999);
-}
+add_action('wp_footer', 'add_js', 999999999);
 
 
 function client_comments_slider($args, $content)
